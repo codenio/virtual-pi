1f48fbc6ac5433298faeb077df940c24ea504357
codenio <aananthraj1995@gmail.com>
2021-04-20 16:06:06 +0530

    Bugfix busio
    This Commit,
    - Fixes missing scan method in busio

--- VPi/busio.py
+++ VPi/busio.py
@@ -1,3 +1,5 @@
 class I2C:
     def __init__(self, scl, sda, frequency=400000):
         print(f"I2C confifure using SCL: {scl}, SDA: {sda}, at freq: {frequency}")
+    def scan(self):
+        return []
